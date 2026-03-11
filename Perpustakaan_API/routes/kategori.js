036d471415db17b736dea0573f80f957f37df83a
HyuzaNuzumi <linglegendstwo@gmail.com>
2026-03-11 08:07:31 +0800

    feat: tambahkan rute delete buku berdasarkan id_kategori

--- Perpustakaan_API/routes/kategori.js
+++ Perpustakaan_API/routes/kategori.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const PrismaClient = require('../src/lib/prisma');
 const prisma = require('../src/lib/prisma');
 
 router.get('/', async (req, res) => {
@@ -12,8 +11,9 @@ router.get('/', async (req, res) => {
     }
 });
 
+//tambah kategori
 router.post('/', async (req,res) => {
-    const { nama_kategori } =req.body;
+    const { nama_kategori } = req.body;
     try {
         const kategori_baru = await prisma.kategori.create({
             data: { nama_kategori}
@@ -23,5 +23,77 @@ router.post('/', async (req,res) => {
         res.status(400).json({ error: error.message });
     }
 });
+
+
+//tampilkan kategori
+router.get('/:id', async(req,res) => {
+    const { id } = req.params;
+    try {
+        const kategori = await prisma.kategori.findUnique({
+            where : { id_kategori: Number(id)},
+        });
+        if(!kategori) {
+            return res.status(404).json({ message: 'Kategori tersebut tidak ada'})
+        }
+        res.json(kategori)
+    } catch (error) {
+        res.status(500).json({eror: error.message})
+    }
+});
+
+//update kategori
+router.put("/:id", async(req,res) => {
+    const { id } = req.params;
+    const { nama_kategori } = req.body;
+    try {
+        const updateKategori = await prisma.kategori.update({
+            where: {id_kategori: Number(id)},
+            data: {
+                nama_kategori
+            }
+        });
+        res.json(updateKategori)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+});
+
+//detelete berdasarkan id
+router.delete('/:id', async(req, res)=> {
+    const { id } = req.params;
+    try{
+        await prisma.kategori.delete({
+            where: { id_kategori: Number(id)}
+        
+        });
+        return res.status(200).json({ message: `Kategori dengan ID ${id} berhasil dihapus!`})
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+
+//delete buku berdasarkan id_Kategori
+router.delete('/kategori/:id_kategori', async(req,res) => {
+    const {id_kategori } = req.params;
+    try {
+        // simpan hasil penghapusan ke variabel "deleteKategori"
+        const deleteKategori = await prisma.kategori.deleteMany({
+            where: { id_kategori: Number(id_kategori)}
+        });
+         
+        //check apakah ada data yang akan dihapus
+        if( deleteKategori.count === 0 ){
+            return res.status(404).json({ message: "Tidak ada kategori dengan ID tersebut" });
+        } 
+        res.json({
+            message: `Berhasil menghapus ${deleteKategori.count} kategori dengan ID ${id_kategori}`
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message})
+    }
+})
+
+
 
 module.exports = router;
--- Perpustakaan_API/server.js
+++ Perpustakaan_API/server.js
@@ -3,15 +3,20 @@ const app = express();
 const port = 3000;
 
 //kategori
-const kategori = require('./routes/kategori')
+const kategoriRoutes = require('./routes/kategori')
+//buku
+const bukuRoutes = require('./routes/buku')
 
 //middleware (express bisa membaca JSON dari bodyrequest)
-app.use(express.json)
+app.use(express.json());
 
 //router kategori
-app.get('/', (req, res) => {
-    res.send('API Perpustakaan Aktif !!!')
-})
+app.use('/kategori', kategoriRoutes);
+
+//router buku
+app.use('/buku', bukuRoutes);
+
+
 
 app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}`);
